1d41a7dfbb64e53ed90ca798fd57bca701061b60
jinyunzhao <jinyun.zhao@wuxcl.com>
2018-08-01 14:35:30 +0800

    question 4-Median of Two Sorted Arrays

--- src/algorithm/MedianofTwoSortedArrays.java
+++ src/algorithm/MedianofTwoSortedArrays.java
@@ -3,7 +3,7 @@ package algorithm;
 public class MedianofTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1 = nums1.length, len2 = nums2.length;
-        if ((len1 + len2) / 2 == 0) {
+        if ((len1 + len2) % 2 == 0) {
             return (findKth(nums1, nums2, 0, len1, 0, len2, (len1 + len2) / 2)
                     + findKth(nums1, nums2, 0, len1, 0, len2, (len1 + len2) / 2 + 1)
             ) / 2;
@@ -16,7 +16,13 @@ public class MedianofTwoSortedArrays {
         if (len1 > len2) {
             return findKth(nums2, nums1, beg2, len2, beg1, len1, k);
         }
-        int p = k / 2;
+        if (len1 == 0) {
+            return nums2[beg2 + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[beg1], nums2[beg2]);
+        }
+        int p = Math.min(k / 2, len1);
         int q = k - p;
         if (nums1[beg1 + p - 1] == nums2[beg2 + q - 1]) {
             return nums1[beg1 + p - 1];
@@ -25,5 +31,11 @@ public class MedianofTwoSortedArrays {
         } else {
             return findKth(nums1, nums2, beg1 + p, len1 - p, beg2, len2, k - p);
         }
+    }
+
+    public static void main(String[] args) {
+        MedianofTwoSortedArrays test = new MedianofTwoSortedArrays();
+        int[] nums2 = {1}, nums1 = {2, 3, 4, 5};
+        System.out.println(test.findMedianSortedArrays(nums1, nums2));
     }
 }
