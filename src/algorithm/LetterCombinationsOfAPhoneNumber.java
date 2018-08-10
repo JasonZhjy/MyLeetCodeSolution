28e82b4df97eeaa251cbc44e6a647f3c22c71198
jinyunzhao <jinyun.zhao@wuxcl.com>
2018-08-10 18:56:34 +0800

    daizuo

---
+++ src/algorithm/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.*;
+
+public class LetterCombinationsOfAPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+        String dict[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "qps", "tuv", "wxyz"};
+        List<String> rs = new ArrayList<>();
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        LetterCombinationsOfAPhoneNumber test = new LetterCombinationsOfAPhoneNumber();
+
+    }
+}
