0cc0d981488bc3c6354903a05a14583ecccd01ad
jinyunzhao <jinyun.zhao@wuxcl.com>
2018-08-16 10:39:49 +0800

    26. Remove Duplicates from Sorted Array -update

--- src/algorithm/RemoveDuplicatesFromSortedArray.java
+++ src/algorithm/RemoveDuplicatesFromSortedArray.java
@@ -1,9 +1,6 @@
 package algorithm;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
 
 /**
  * 26. Remove Duplicates from Sorted Array
