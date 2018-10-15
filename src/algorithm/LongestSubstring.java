84551b02a85fe9e5d6c7035e2edbf1c4607a2f36
jinyunzhao <jinyun.zhao@wuxcl.com>
2018-10-15 18:08:29 +0800

    update 3

--- src/algorithm/LongestSubstring.java
+++ src/algorithm/LongestSubstring.java
@@ -11,7 +11,7 @@ public class LongestSubstring {
             int index;
             if ((index = str.lastIndexOf(s.charAt(right) + "")) > -1) {
                 len = right - (index + left);
-                left += index;
+                left += index + 1;
             } else {
                 len++;
             }
@@ -21,9 +21,28 @@ public class LongestSubstring {
         return maxLen;
     }
 
+    public int lengthOfLongestSubstring2(String s) {
+        if ("".equals(s) || s == null) return 0;
+        int left = 0, right = 0, maxLen = 1, len = 0;
+        while (right++ < s.length() - 1) {
+            String str = s.substring(left, right);
+            int index;
+            if ((index = str.lastIndexOf(s.charAt(right) + "")) > -1) {
+                len = str.length();
+                left = left + index + 1;
+            } else {
+                len = right - left + 1;
+            }
+            maxLen = Math.max(len, maxLen);
+        }
+
+        return maxLen;
+    }
+
+
     public static void main(String[] args) {
         LongestSubstring test = new LongestSubstring();
-        System.out.println(test.lengthOfLongestSubstring("abcade"));
+        System.out.println(test.lengthOfLongestSubstring2("pwwkew"));
     }
 
 }
