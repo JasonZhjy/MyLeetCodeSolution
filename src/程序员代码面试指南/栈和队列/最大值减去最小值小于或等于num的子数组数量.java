9eb67c67507a8bf3384475e2e06552a554811fb3
jinyunzhao <jinyun.zhao@wuxcl.com>
2019-03-07 10:22:36 +0800

    算法指南-栈和队列

--- src/程序员代码面试指南/栈和队列/最大值减去最小值小于或等于num的子数组数量.java
+++ src/程序员代码面试指南/栈和队列/最大值减去最小值小于或等于num的子数组数量.java
@@ -1,4 +1,45 @@
 package 程序员代码面试指南.栈和队列;
 
+import java.util.LinkedList;
+
 public class 最大值减去最小值小于或等于num的子数组数量 {
+    public int getNum (int[] arr, int num) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        LinkedList<Integer> qmin = new LinkedList<>();
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int i = 0, j = 0, res = 0;
+
+        while (i < arr.length) {
+            while (j < arr.length) {
+                while (!qmin.isEmpty() && arr[qmin.peekLast()] >= arr[j]) {
+                    qmin.pollLast();
+                }
+                qmin.addLast(j);
+
+                while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[j]) {
+                    qmax.pollLast();
+                }
+                qmax.addLast(j);
+
+                if (arr[qmax.getFirst()] - arr[qmin.getFirst()] > num) {
+                    break;
+                }
+                j++;
+            }
+
+            if (qmin.peekFirst() == i) {
+                qmin.pollFirst();
+            }
+            if (qmax.peekFirst() == i) {
+                qmax.pollFirst();
+            }
+
+            res += j - i;
+            i++;
+        }
+        return res;
+    }
 }
