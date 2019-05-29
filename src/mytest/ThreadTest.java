c17080ca0ec18466cf9022e5c20bc99cdef4fc9d
libery zhao <z634829234@163.com>
2019-05-29 22:40:39 +0800

    qwq

--- src/mytest/ThreadTest.java
+++ src/mytest/ThreadTest.java
@@ -7,6 +7,7 @@ public class ThreadTest {
     public static void main (String[] args) {
         int[] arr = new int[2];
         int[] as = Arrays.copyOf(arr, 2);
+         as = Arrays.copyOf(arr, 2);
 
     }
 
